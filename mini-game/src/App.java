19753149f054dfcf4fa151efb4ef8144e0cfced1
miguelsacardo <miguelsacardo3@gmail.com>
2024-12-13 11:16:35 -0300

    beginnig of the guild system. 13/12/2024

--- mini-game/src/App.java
+++ mini-game/src/App.java
@@ -6,15 +6,47 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        Player p = new Player();
+        Player newPlayer = new Player();
         //Enemy e = new Enemy();
-        p.exibirAtributos();
+        // p.exibirAtributos();
        
         while(true){
-            System.out.println("Escreva o nome do seu jogador: ");
-            p.namePlayer = input.nextLine();
 
-            System.out.println("Nome do jogador: " + p.namePlayer);
+            //introducing the story for the player
+            System.out.println("\n----------------***---------------");
+            System.out.println("Hello Adventurer! Your help is claimed in the north Kingdom.\nThe north Kingdom is known by your great gold reservations\nAnd the enemies are comming to stole it!\nYou need to stop them!");
+            System.out.println("----------------***---------------");
+
+            //player's name input
+            System.out.println("Hero, write your name!");
+            newPlayer.namePlayer = input.nextLine().toUpperCase();
+            
+            System.out.println("----------------***---------------");
+            
+
+            //testing function
+            newPlayer.exibirAtributos();
+            System.out.println("\n----------------***---------------");
+
+            //MAYBE I SHOULD PUT A LOOP HERE
+            System.out.println("Choose your guild:\nClothfeet\nWilsons\nStronghand\nEagles\nYour Choice: ");
+            String choiceGuild = input.nextLine().toUpperCase();
+
+            if (choiceGuild.equals("CLOTHFEET") || choiceGuild.equals("WILSONS") || choiceGuild.equals("STRONGHAND") ||choiceGuild.equals("EAGLES")){
+                newPlayer.chooseGuild(choiceGuild);
+                newPlayer.exibirAtributos();
+            }
+            else{
+                System.out.println("Choose a valid guild!");
+                continue;
+            }
+            /////////////////////////////////
+            // System.out.println(guild);
+    
+            // p.namePlayer = input.nextLine();
+
+            // System.out.println("Nome do jogador: " + p.namePlayer);
+            input.close();
             break;
             
         }
@@ -27,12 +59,32 @@ class Player{
     public int level = 1;
     public int armorClass = 15;
 
-    public Double attack = 10.0;
-    public Double defense = 10.0;
+    public double attack = 10;
+    public double defense = 10;
+
+    public String guilds[] = {"CLOTHFEET","WILSONS","STRONGHAND","EAGLES"};
+
 
 
     public void exibirAtributos(){
-        System.out.printf("Life: %d\nLevel: %d\nAttack: %f\nDefense: %f", this.life, this.level, this.attack, this.defense);
+        System.out.printf("Name: %s\nArmor Class: %d\nLife: %d\nLevel: %d\nAttack: %.0f\nDefense: %.0f",this.namePlayer,this.armorClass,this.life, this.level, this.attack, this.defense);
+    }
+
+    public void chooseGuild(String playerGuild){
+        for(String guild : guilds){
+            if(playerGuild.equals(guild)){ //I must use equals because guild is an object
+                System.out.printf("Your guild is: %s", playerGuild);
+
+                switch (playerGuild) {
+                    case "CLOTHFEET":
+                        this.life += 5;
+                        this.armorClass += 2;
+                        this.attack += 5;
+                        this.defense += 5;
+                        break;
+                }
+            }
+        }
     }
 }
 
